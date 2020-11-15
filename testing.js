e9250a68d598a19a8b7ad3c3ca9c0fea2a6ced1e
Matija0408 <msekelj@gmail.com>
2020-11-15 19:16:10 +0000

    Main page spelling correction
    Button at the end of page

--- testing.js
+++ testing.js
@@ -1,6 +1,31 @@
-var toggleButton = document.getElementsByClassName('toggle-button')[0];
-var navbarLinks = document.getElementsByClassName('navbar-links')[0];
+const myHeading = document.querySelector('h1');
+myHeading.textContent = "Hello World!";
 
-toggleButton.addEventListener('click', () => {
-  navbarLinks.classList.toggle('active');
-})
+
+
+
+
+
+
+
+// var i = 0;
+// var images = ["assets/img/brownie-548591_1920.jpg", "assets/img/swede-cakes-2123191_1920.jpg"];
+// var time = ;
+
+// // images[0]= "assets/img/brownie-548591_1920.jpg";
+// // images[1]= "assets/img/swede-cakes-2123191_1920.jpg";
+
+
+// function changeImg(){
+//   document.slike.src = images[i];
+
+//   if(i < images.length -1){
+//     i++;
+//   }else{
+//     i = 0;
+//   }
+
+//   setTimeout("changeImg()", time);
+  
+// }
+// window.onload = changeImg();
